8b60e7cfc2d14b646a1009c8781ebb9f619a4f83
Andres Fernandez-Murray <andres.fmurray@gmail.com>
2021-07-14 16:56:05 -0400

    fix ssl

--- src/db/index.js
+++ src/db/index.js
@@ -1,9 +1,6 @@
 const pg = require("pg");
 
-const client = new pg.Client({
-  connectionString: process.env.DATABASE_URL || "",
-  ssl: false,
-});
+const client = new pg.Client({   connectionString: process.env.DATABASE_URL || "",   ssl: process.env.DATABASE_URL ? { rejectUnauthorized: false } : false });
 
 client
   .connect()
